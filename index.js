f3b6e47a7740df2a2412dc2b414a7f374f780c16
Yan <87247616+NigglyNoodle@users.noreply.github.com>
2023-04-15 15:26:17 +0100

    Change to stitch and row counters with reset

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-let countEl = document.getElementById("count-el")
-let myCount = 0
+// let countEl = document.getElementById("count-el")
+// let myCount = 0
 
+// let saveEl = document.getElementById("save-el")
 
+// console.log(saveEl)
 
-let saveEl = document.getElementById("save-el")
+// function increment() {
+//     myCount += 1
+//     countEl.textContent = myCount
+// }
 
-console.log(saveEl)
+// function save() {
+//     let mySavedCount = myCount + " - " 
+//     saveEl.textContent += mySavedCount
+//     myCount = 0
+//     countEl.textContent = myCount
+// }
 
-function increment() {
-    myCount += 1
-    countEl.textContent = myCount
+let countStitch = document.getElementById("count-stitch")
+let stitches = 0
+
+function incrementStitch() {
+    stitches += 1
+    countStitch.textContent = stitches
 }
 
-function save() {
-    let mySavedCount = myCount + " - "
-    saveEl.textContent += mySavedCount
-    myCount = 0
-    countEl.textContent = myCount
+function resetStitch() {
+    stitches = 0
+    countStitch.textContent = stitches
 }
 
+let countRow = document.getElementById("count-row")
+let rows = 0
+
+function incrementRow() {
+    rows += 1
+    countRow.textContent = rows
+}
+
+function resetRow() {
+    rows = 0
+    countRow.textContent = rows
+}
